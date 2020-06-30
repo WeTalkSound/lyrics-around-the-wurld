862072cfd21ba4c4661ef6aa4e25ea6c19c2cd77
Etinosa Obaseki <etinosaobaseki@gmail.com>
2020-06-30 19:54:09 +0000

    Setup Wurld Challenge

--- src/Components/Utilities/Header/Header.js
+++ src/Components/Utilities/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import logo from './header-logo.png'
+import logo from './header-logo.jpg'
 
 export default function Header() {
   return (
     <div className="firstcon text-center justify-content-center">
       <Link to="/">
-        <img src={ logo } alt="Bolt Protect" style={{ height: "200px" }} className="headerlogo mx-auto d-flex img-fluid" />
+        <img src={ logo } alt="Bolt Protect" style={{ height: "200px", borderRadius: "50%" }} className="headerlogo mx-auto d-flex img-fluid" />
       </Link>
     </div>
   )
